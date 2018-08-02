ceb6d4ceb5c1161715e59aa6740f4d656515b863
Wenny Chen <867820703@qq.com>
2018-08-02 21:21:13 +0800

    Create Code.java

---
+++ testing.git/src/main/java/testing/git/testing/git/Code.java
@@ -0,0 +1,8 @@
+public class Code 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "for code mergingss!" );
+    }
+}
