a658d466953d55f5567b6f0e5a4827e460cb5b20
Wenny Chen <867820703@qq.com>
2018-08-02 21:28:02 +0800

    Create Code1.java

---
+++ testing.git/src/main/java/testing/git/testing/git/Code1.java
@@ -0,0 +1,11 @@
+package testing.git.testing.git;
+
+public class Code1 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "for code mergingss!" );
+        System.out.println( "for code mergingss!!" );
+    }
+}
